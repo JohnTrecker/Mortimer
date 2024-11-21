228321548c4a72bfa3ea164a149b5d03a659d316
John Trecker <jttrecker@gmail.com>
2024-11-21 15:15:29 -0600

    fix lint errors

--- components/Pie/Pie.tsx
+++ components/Pie/Pie.tsx
@@ -71,14 +71,14 @@ export default function PieChart({
     const subtopicSegment = (_) => 0.2;
 
     // color scales
-    const categoryRange = [...[...Array(10).keys()].map((i) => `rgba(93,30,91,${i === 0 ? 1.0 : 0.1 * i})`)]
+    const categoryRange = Array.from({ length: 10 }, (_, i) => `rgba(93,30,91,${i === 0 ? 1.0 : 0.1 * i})`)
     const getCategoryColor = scaleOrdinal({
         domain: categories.map((c) => c.id),
         range: categoryRange,
     });
     // returns a continoius range of colors from white to white
     function getContinuiousRange(n: number) { 
-        const firstHalf = [...Array(Math.round(n/2 + 1)).keys()].slice(1)
+        const firstHalf = Array.from({ length: Math.round(n / 2) }, (_, i) => i + 1);
         const secondHalf = [...firstHalf].reverse()
         return [...firstHalf, ...secondHalf]    
     }
@@ -392,10 +392,9 @@ function AnimatedPie<Datum>({
     const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.2;
     
     // subtopic specific logic
-    const isSubtopic = Boolean(arc.data?.subtopic)
-    const isCategory = Boolean(arc.data?.category)
-    const isSubtopicHovered = Boolean(hoveredSubtopic?.id === arc.data?.id)
-
+      const isSubtopic = 'subtopic' in (arc.data as any);
+      const isCategory = 'category' in (arc.data as any);
+      const isSubtopicHovered = Boolean(hoveredSubtopic?.id === (arc.data as any)?.id);
     // text display
     const x = isCategory ? centroidX * 1.8 : centroidX
     const y = isCategory ? centroidY * 1.8 : centroidY
