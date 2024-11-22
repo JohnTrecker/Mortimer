5579265e2a6e09f1335356de9651bc9074a14af1
John Trecker <jttrecker@gmail.com>
2024-11-22 11:35:33 -0600

    deprecate useSupabase hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from 'react'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { Session } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-import useSupabase from '@/hooks/useSupabase'
 import '@/styles/globals.css'
 
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_API_KEY,
+)
+
 function MyApp({ Component, pageProps }) {
-  const supabase = useSupabase()
   const [session, setSession] = useState<Session | null>(null)
 
   useEffect(() => {
@@ -17,20 +21,19 @@ function MyApp({ Component, pageProps }) {
       })
       .catch(() => setSession(null))
 
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session)
     })
 
     return () => subscription.unsubscribe()
   }, [])
 
-  if (!session) {
-    return (
-      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-    )
-  }
+  if (!supabase) return null
+  // if (!session) {
+  //   return (
+  //     <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+  //   )
+  // }
   return <Component session={session} supabase={supabase} {...pageProps} />
 }
 
