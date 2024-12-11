210d263cb7c0e61f69481a6830eea6e3e8fc230e
John Trecker <jttrecker@gmail.com>
2024-12-11 17:35:30 -0600

    swap key

--- context/supabase.tsx
+++ context/supabase.tsx
@@ -12,7 +12,7 @@ const SupabaseContext = createContext(init)
 
 export const supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_API_KEY!
 )
 
 function SupabaseProvider({ children }: { children: React.ReactNode }) {
