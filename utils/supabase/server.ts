34d6a936c7c27894708c90819283428546a3f877
John Trecker <jttrecker@gmail.com>
2024-12-21 11:20:49 -0600

    remove deprecated server client methods

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -9,29 +9,6 @@ export async function createClient(cookieStore = cookies()) {
         process.env.NEXT_PUBLIC_SUPABASE_API_KEY!,
         {
             cookies: {
-                get(name: string) {
-                    return resolvedCookies.get(name)?.value
-                },
-                set(name: string, value: string, options: CookieOptions) {
-                    try {
-                        resolvedCookies.set({ name, value, ...options })
-                    } catch (error) {
-                        console.error('Error setting cookies in server: ', error)
-                        // The `set` method was called from a Server Component.
-                        // This can be ignored if you have middleware refreshing
-                        // user sessions.
-                    }
-                },
-                remove(name: string, options: CookieOptions) {
-                    try {
-                        resolvedCookies.set({ name, value: '', ...options })
-                    } catch (error) {
-                        console.error('Error removing cookies in server: ', error)
-                        // The `delete` method was called from a Server Component.
-                        // This can be ignored if you have middleware refreshing
-                        // user sessions.
-                    }
-                },
                 getAll() {
                     return resolvedCookies.getAll()
                 },
